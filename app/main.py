a86aa4d03ff56b399635c3b862c67dd38acc7014
Niels Luecking <niels.luecking@rwth-aachen.de>
2020-09-05 11:38:11 +0200

    api migration

--- app/main.py
+++ app/main.py
@@ -1,72 +1,119 @@
+import json
 import os
-import random
+import bottle
+import brain2
+import time
+import sys
+import Game_object_pool
+from api import ping_response, start_response, move_response, end_response
 
-import cherrypy
+# TODO turn back to true before commit
+main_print = True
 
-"""
-This is a simple Battlesnake server written in Python.
-For instructions see https://github.com/BattlesnakeOfficial/starter-snake-python/README.md
-"""
+def remove_shouts(data):
+    for snake in data['board']['snakes']:
+        snake.pop('shout', None)
+    data['you'].pop('shout', None)
 
+@bottle.route('/')
+def index():
+    return '''
+    Battlesnake documentation can be found at
+       <a href="https://docs.battlesnake.io">https://docs.battlesnake.io</a>.
+    '''
 
-class Battlesnake(object):
-    @cherrypy.expose
-    @cherrypy.tools.json_out()
-    def index(self):
-        # This function is called when you register your Battlesnake on play.battlesnake.com
-        # It controls your Battlesnake appearance and author permissions.
-        # TIP: If you open your Battlesnake URL in browser you should see this data
-        return {
-            "apiversion": "1",
-            "author": "",  # TODO: Your Battlesnake Username
-            "color": "#888888",  # TODO: Personalize
-            "head": "default",  # TODO: Personalize
-            "tail": "default",  # TODO: Personalize
-        }
+@bottle.route('/static/<path:path>')
+def static(path):
+    """
+    Given a path, return the static file located relative
+    to the static folder.
 
-    @cherrypy.expose
-    @cherrypy.tools.json_in()
-    def start(self):
-        # This function is called everytime your snake is entered into a game.
-        # cherrypy.request.json contains information about the game that's about to be played.
-        # TODO: Use this function to decide how your snake is going to look on the board.
-        data = cherrypy.request.json
+    This can be used to return the snake head URL in an API response.
+    """
+    return bottle.static_file(path, root='static/')
 
-        print("START")
-        return "ok"
+@bottle.post('/ping')
+def ping():
+    """
+    A keep-alive endpoint used to prevent cloud application platforms,
+    such as Heroku, from sleeping the application instance.
+    """
+    return ping_response()
 
-    @cherrypy.expose
-    @cherrypy.tools.json_in()
-    @cherrypy.tools.json_out()
-    def move(self):
-        # This function is called on every turn of a game. It's how your snake decides where to move.
-        # Valid moves are "up", "down", "left", or "right".
-        # TODO: Use the information in cherrypy.request.json to decide your next move.
-        data = cherrypy.request.json
+@bottle.post('/start')
+def start():
+    global previous_data
+    data = bottle.request.json
+    remove_shouts(data)
+    previous_data = data
+    Game_object_pool.GamePool().init_pool(data)
 
-        # Choose a random direction to move in
-        possible_moves = ["up", "down", "left", "right"]
-        move = random.choice(possible_moves)
+    """
+    TODO: If you intend to have a stateful snake AI,
+            initialize your snake state here using the
+            request's data if necessary.
+    """
+    print("starting")
+    print(json.dumps(data))
 
-        print("MOVE: {move}")
-        return {"move": move}
+    color = "#00FF00"
 
-    @cherrypy.expose
-    @cherrypy.tools.json_in()
-    def end(self):
-        # This function is called when a game your snake was in ends.
-        # It's purely for informational purposes, you don't have to make any decisions here.
-        data = cherrypy.request.json
-
-        print("END")
-        return "ok"
+    return start_response(color)
 
 
-if __name__ == "__main__":
-    server = Battlesnake()
-    cherrypy.config.update({"server.socket_host": "0.0.0.0"})
-    cherrypy.config.update(
-        {"server.socket_port": int(os.environ.get("PORT", "8080")),}
+@bottle.post('/move')
+def move():
+    if main_print:
+        print("Received move request, unpacking data")
+    data = bottle.request.json
+    remove_shouts(data)
+    if main_print:
+        print("Received move request ", data['turn'])
+    my_move_response = get_move_response_string(data)
+    return move_response(my_move_response)
+
+
+state_list = []
+def get_move_response_string(data):
+    start_time = time.time()
+
+    response2 = brain2.get_best_move_based_on_current_data(data)
+
+    if type(response2) == str:
+        response2 = (response2, [])
+
+    my_move_response2, move_score_list2 = response2
+
+    if main_print:
+        print("response time", time.time()-start_time, "name", data['you']['name'])
+        print("Responded to move request ", data['turn'], " with ", my_move_response2, move_score_list2)
+    return my_move_response2
+
+
+@bottle.post('/end')
+def end():
+    data = bottle.request.json
+    remove_shouts(data)
+
+    """
+    TODO: If your snake AI was stateful,
+        clean up any stateful objects here.
+    """
+    print("end")
+    print(json.dumps(data))
+
+    return end_response()
+
+# Expose WSGI app (so gunicorn can find it)
+application = bottle.default_app()
+
+if __name__ == '__main__':
+    port = 8080
+    if len(sys.argv) == 2:
+        port += int(sys.argv[1])
+    bottle.run(
+        application,
+        host=os.getenv('IP', '0.0.0.0'),
+        port=os.getenv('PORT', str(port)),
+        debug=os.getenv('DEBUG', True)
     )
-    print("Starting Battlesnake Server...")
-    cherrypy.quickstart(server)
